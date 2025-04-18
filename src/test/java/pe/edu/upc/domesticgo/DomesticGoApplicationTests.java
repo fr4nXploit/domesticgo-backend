202592128b3b8e16c13988946f32b55d05ead3fe
brunohean <u201514035@upc.edu.pe>
2025-04-18 11:41:42 -0500

    Proyecto Base - Packages (vacio) y Properties

---
+++ src/test/java/pe/edu/upc/domesticgo/DomesticGoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.domesticgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomesticGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
